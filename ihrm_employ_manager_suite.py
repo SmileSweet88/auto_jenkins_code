d4b7b93b9673388083a2237e5206c066dff7c454
SmileSweet88 <353201107@qq.com>
2019-10-17 16:49:07 +0800

    shabbbsjvksdn2

--- ihrm_employ_manager_suite.py
+++ ihrm_employ_manager_suite.py
@@ -11,19 +11,16 @@ from case.test_ihrm_login import TestIhrmLogin
 from constant.constant import workPath, log_config
 
 log_config()
-try:
-    suit = unittest.TestSuite()
-    suit.addTest(unittest.makeSuite(TestIhrmLogin))
-    suit.addTest(unittest.makeSuite(TestIhrmEmpoly))
-    # fileName = "{}ihrm_employ.html".format(time.strftime("%Y%m%d%H%M%S"))
-    # fileName = "report.html"
-    # filePath = workPath + "/report/"
-    report_path = "./report/report.html"
-    with open(report_path, "wb") as f:
-        # 实例化HTMLTestRunner对象，传入报告文件流f
-        runner = HTMLTestRunner(stream=f, title="test_Hr_employ", description="test_my")
-    runner.run(suit)
-    # BeautifulReport(suit).report(filename=fileName, description="hello", log_path=filePath)
-except Exception as es:
-    print("**************")
-    logging.info(es)
+
+suit = unittest.TestSuite()
+suit.addTest(unittest.makeSuite(TestIhrmLogin))
+suit.addTest(unittest.makeSuite(TestIhrmEmpoly))
+# fileName = "{}ihrm_employ.html".format(time.strftime("%Y%m%d%H%M%S"))
+# fileName = "report.html"
+# filePath = workPath + "/report/"
+report_path = "./report/report.html"
+with open(report_path, "wb") as f:
+    # 实例化HTMLTestRunner对象，传入报告文件流f
+    runner = HTMLTestRunner(stream=f, title="test_Hr_employ", description="test_my")
+runner.run(suit)
+# BeautifulReport(suit).report(filename=fileName, description="hello", log_path=filePath)
