57729d984ee623bf1733f25ace6c410ebab590f4
SmileSweet88 <353201107@qq.com>
2019-10-17 16:46:34 +0800

    shabbbsjvksdn

--- ihrm_employ_manager_suite.py
+++ ihrm_employ_manager_suite.py
@@ -4,6 +4,8 @@ import unittest
 from BeautifulReport import BeautifulReport
 import logging
 
+from HTMLTestRunner import HTMLTestRunner
+
 from case.test_ihrm_employ import TestIhrmEmpoly
 from case.test_ihrm_login import TestIhrmLogin
 from constant.constant import workPath, log_config
@@ -14,9 +16,14 @@ try:
     suit.addTest(unittest.makeSuite(TestIhrmLogin))
     suit.addTest(unittest.makeSuite(TestIhrmEmpoly))
     # fileName = "{}ihrm_employ.html".format(time.strftime("%Y%m%d%H%M%S"))
-    fileName = "report.html"
-    filePath = workPath + "/report/"
-    BeautifulReport(suit).report(filename=fileName, description="hello", log_path=filePath)
+    # fileName = "report.html"
+    # filePath = workPath + "/report/"
+    report_path = "./report/report.html"
+    with open(report_path, "wb") as f:
+        # 实例化HTMLTestRunner对象，传入报告文件流f
+        runner = HTMLTestRunner(stream=f, title="test_Hr_employ", description="test_my")
+    runner.run(suit)
+    # BeautifulReport(suit).report(filename=fileName, description="hello", log_path=filePath)
 except Exception as es:
     print("**************")
     logging.info(es)
