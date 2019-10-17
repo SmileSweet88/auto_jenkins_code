39790365e46cfb05d412dd974067acae00f5afe0
SmileSweet88 <353201107@qq.com>
2019-10-17 16:41:23 +0800

    shabbb

--- ihrm_employ_manager_suite.py
+++ ihrm_employ_manager_suite.py
@@ -16,7 +16,7 @@ try:
     # fileName = "{}ihrm_employ.html".format(time.strftime("%Y%m%d%H%M%S"))
     fileName = "report.html"
     filePath = workPath + "/report/"
-    BeautifulReport(suit).report(filename=fileName, description="员工模块：增删改查", log_path=filePath)
+    BeautifulReport(suit).report(filename=fileName, description="hello", log_path=filePath)
 except Exception as es:
     print("**************")
     logging.info(es)
