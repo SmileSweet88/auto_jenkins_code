1fe9fac12bbbb1e9b3480240e674ddd2c425662c
SmileSweet88 <353201107@qq.com>
2019-10-17 17:32:29 +0800

    shabbbsjvksdn27776  ---0

--- ihrm_employ_manager_suite.py
+++ ihrm_employ_manager_suite.py
@@ -3,16 +3,20 @@ import os
 
 import unittest
 import HTMLTestRunner
+from BeautifulReport import BeautifulReport
 
 from case.test_ihrm_employ import TestIhrmEmpoly
 from case.test_ihrm_login import TestIhrmLogin
+from constant.constant import workPath
 
 suit = unittest.TestSuite()
 suit.addTest(unittest.makeSuite(TestIhrmLogin))
-# suit.addTest(unittest.makeSuite(TestIhrmEmpoly))
+suit.addTest(unittest.makeSuite(TestIhrmEmpoly))
+filePath = workPath + "/report"
+BeautifulReport(suit).report(filename="report.html",log_path=filePath,description="test_employ")
 
-path1 = os.path.dirname(os.path.abspath(__file__))
+# path1 = os.path.dirname(os.path.abspath(__file__))
 
-with open(path1 + '/report/report.html', 'wb') as f:
-    runner = HTMLTestRunner.HTMLTestRunner(f, title='text1', description='zzz')
-    runner.run(suit)
+# with open(path1 + '/report/report.html', 'wb') as f:
+#     runner = HTMLTestRunner.HTMLTestRunner(f, title='text1', description='zzz')
+#     runner.run(suit)
